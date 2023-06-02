6d1af7d5509be4734df568fcbfc2a8bc8c7b8929
Amer Barnawi <barnawi.amer@yahoo.com>
2023-06-02 15:15:02 +0200

    Creating the database and the user model is done.

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
+from db import models
+from db.database import engine
 
 app = FastAPI()
+
 
 @app.get("/")
 def hw():
     return "Hello world"
+
+
+models.Base.metadata.create_all(engine)
