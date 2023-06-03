af103223f169949b0ce326aace98131d6f58472c
Amer Barnawi <barnawi.amer@yahoo.com>
2023-06-03 19:55:11 +0200

    Adding Get All Comments Endpoint & Delete Comment Endpoint.

--- db/db_comment.py
+++ db/db_comment.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from db.models import DbComment
 from routers.schemas import CommentBase
 from sqlalchemy.orm.session import Session
@@ -15,3 +16,21 @@ def create_comment(db: Session, request: CommentBase):
     db.commit()
     db.refresh(new_comment)
     return new_comment
+
+
+def get_all_comments(db: Session):
+    return db.query(DbComment).all()
+
+
+def delete_comment(db: Session, comment_id, username):
+    comment = db.query(DbComment).filter(DbComment.id == comment_id).first()
+    if not comment:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f'Comment by id:{comment_id} is not found!')
+    if comment.username != username:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
+                            detail='Only comment creater can delete the comment!')
+
+    db.delete(comment)
+    db.commit()
+    return 'Comment has been deleted successfully!'
--- routers/comment.py
+++ routers/comment.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Depends
 from auth.oauth2 import get_current_user
 from db.database import get_db
@@ -14,3 +15,13 @@ router = APIRouter(
 @router.post('', response_model=CommentDisplay)
 def create_comment(request: CommentBase, db: Session = Depends(get_db), current_user: UserAuth = Depends(get_current_user)):
     return db_comment.create_comment(db, request)
+
+
+@router.get('/all-comments', response_model=List[CommentDisplay])
+def get_all_comments(db: Session = Depends(get_db)):
+    return db_comment.get_all_comments(db)
+
+
+@router.delete('/{id}')
+def delete_comment(id: str, db: Session = Depends(get_db), current_user: UserAuth = Depends(get_current_user)):
+    return db_comment.delete_comment(db, id, current_user.username)
