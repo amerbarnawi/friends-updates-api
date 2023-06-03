b3dc38570a0fe1f3535ef9329a12ce23c07523d8
Amer Barnawi <barnawi.amer@yahoo.com>
2023-06-03 23:35:30 +0200

    Adding CORS middleware.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from db.database import engine
 from routers import user, post, comment, like
 from fastapi.staticfiles import StaticFiles
 from auth import authentication
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
@@ -22,3 +23,15 @@ def hw():
 models.Base.metadata.create_all(engine)
 
 app.mount('/images', StaticFiles(directory='images'), name='images')
+
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
